89da6753e97be3375b8e3dfc7957fab2fce1a6e6
Carlos Miguel Angulo Mascarell <cangulo@ohpen.com>
2022-06-09 10:49:42 +0200

    fix: OFF-241 solved error when not providing settings-file

--- modules/settings-provider.js
+++ modules/settings-provider.js
@@ -3,20 +3,24 @@ const CONSTANTS = require("./constants.js");
 
 getSettings = () => {
 
+    let acceptedPrefixes, scopes;
+    const defaultPrefixes = CONSTANTS.DEFAULT_CONVENTIONAL_COMMITS
     const settingsFile = process.env.SETTINGS_FILE
 
-    if (!fs.existsSync(settingsFile)) {
+    if (settingsFile && !fs.existsSync(settingsFile)) {
         throw new Error('file provided does not exists')
+    } else if (settingsFile) {
+        const rawData = fs.readFileSync(settingsFile)
+        const jsonData = JSON.parse(rawData)
+        acceptedPrefixes = jsonData?.conventionalCommits
+        scopes = jsonData?.scopes
     }
-
-    const rawData = fs.readFileSync(settingsFile)
-    const jsonData = JSON.parse(rawData)
 
     return {
         targetBranch: process.env.GITHUB_BASE_REF,
         sourceBranch: process.env.GITHUB_HEAD_REF,
-        acceptedPrefixes: jsonData?.conventionalCommits || CONSTANTS.DEFAULT_CONVENTIONAL_COMMITS,
-        scopes: jsonData?.scopes || {}
+        acceptedPrefixes: acceptedPrefixes ?? defaultPrefixes,
+        scopes: scopes ?? {}
     }
 }
 
